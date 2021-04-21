b3948591039ff7df7731785f3147b7d8b943fc93
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-04-21 22:17:56 +0200

    ledger hedger

--- PiScraper/PiScraper.py
+++ PiScraper/PiScraper.py
@@ -1,10 +1,14 @@
+import bisect
 import json
 import os
 
 import requests
 
+
 def getDate(ledgerdate):
-    return int(ledgerdate.split('T')[0].replace('-',''))
+    return int(ledgerdate.split('T')[0].replace('-', ''))
+
+
 def scrapeToCache(link, cachename):
     cache = 'cache\\' + cachename
     if not os.path.exists(cache):
@@ -19,23 +23,36 @@ def scrapeToCache(link, cachename):
     return X
 
 
+def scrapeCarelessly(link):
+    page = requests.get(link)
+    X = json.loads(page.content)
+    return X
+
+
 def scrape_pi_testnet_ledger(ledgerID):
-    ledgerbase = "https://api.testnet.minepi.com/ledgers/" + str(ledgerID)
-    ledgerdict = scrapeToCache(ledgerbase)
+    ledgerID_S = str(ledgerID)
+    ledgerbase = "https://api.testnet.minepi.com/ledgers/" + ledgerID_S
+    ledgerdict = scrapeToCache(ledgerbase, "ldgr_" + ledgerID_S)
     return ledgerdict
 
+
 def getLastLedgerNumber():
-    ledgerbase = "https://api.testnet.minepi.com/ledgers
-    ledgerdict = scrapeToCache(ledgerbase)
-    return 
+    ledgerdict = scrapeCarelessly("https://api.testnet.minepi.com/ledgers?order=desc")
+    return ledgerdict['_embedded']['records'][0]['sequence']
 
 
-def identify_first_at(date, knownDates=dict()):
-
+def identify_greq(date, knownDates: dict):
+    L = list(knownDates.keys())
+    L.sort()
+    V = [knownDates[e] for e in L]
     return
 
 
+def identify_sender(transactionDict):
+
+
 def main():
+    last = getLastLedgerNumber()
     data = scrapeToCache("https://api.testnet.minepi.com/ledgers/1354636", 'ld_1354636.txt')
     return
 
