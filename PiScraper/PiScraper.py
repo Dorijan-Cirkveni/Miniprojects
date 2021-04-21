e31c655ed81d6908407093b92b0b648f16149541
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-04-21 19:50:17 +0200

    lol
    (lots of ledgers)

--- PiScraper/PiScraper.py
+++ PiScraper/PiScraper.py
@@ -1,21 +1,44 @@
 import json
+import os
 
 import requests
 
-
-def main(scrapeNew=False):
-    if scrapeNew:
-        page = requests.get("https://api.testnet.minepi.com/ledgers/1354636/transactions")
-        F = open('picache.txt', 'wb')
+def getDate(ledgerdate):
+    return int(ledgerdate.split('T')[0].replace('-',''))
+def scrapeToCache(link, cachename):
+    cache = 'cache\\' + cachename
+    if not os.path.exists(cache):
+        page = requests.get(link)
+        F = open(cache, 'wb')
         F.write(page.content)
         F.close()
-        Y = json.loads(page.content)
-    F = open('picache.txt', 'rb')
+    F = open(cache, 'rb')
     content = F.read()
     F.close()
     X = json.loads(content)
+    return X
+
+
+def scrape_pi_testnet_ledger(ledgerID):
+    ledgerbase = "https://api.testnet.minepi.com/ledgers/" + str(ledgerID)
+    ledgerdict = scrapeToCache(ledgerbase)
+    return ledgerdict
+
+def getLastLedgerNumber():
+    ledgerbase = "https://api.testnet.minepi.com/ledgers
+    ledgerdict = scrapeToCache(ledgerbase)
+    return 
+
+
+def identify_first_at(date, knownDates=dict()):
+
+    return
+
+
+def main():
+    data = scrapeToCache("https://api.testnet.minepi.com/ledgers/1354636", 'ld_1354636.txt')
     return
 
 
 if __name__ == "__main__":
-    main(True)
+    main()
